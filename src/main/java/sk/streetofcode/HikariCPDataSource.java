7e9e8fa0e3399efc03c4f5643040daed40b2af79
xjahic <jahic.jakub@gmail.com>
2024-01-07 14:00:58 +0100

    Load db connection values from properties

--- src/main/java/sk/streetofcode/HikariCPDataSource.java
+++ src/main/java/sk/streetofcode/HikariCPDataSource.java
@@ -2,22 +2,32 @@ package sk.streetofcode;
 
 import com.zaxxer.hikari.HikariConfig;
 import com.zaxxer.hikari.HikariDataSource;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 
 import java.sql.Connection;
 import java.sql.SQLException;
+import java.util.Properties;
 
 public class HikariCPDataSource {
 
     private static final HikariConfig config = new HikariConfig();
     private static final HikariDataSource ds;
 
+    private static final Logger logger = LoggerFactory.getLogger(HikariCPDataSource.class);
+
+
     static {
-        config.setJdbcUrl("jdbc:mysql://localhost:3306/contacts");
-        config.setUsername("root");
-        config.setPassword("password");
-        config.addDataSourceProperty("cachePrepStmts", "true");
-        config.addDataSourceProperty("prepStmtCacheSize", "250");
-        config.addDataSourceProperty("prepStmtCacheSqlLimit", "2048");
+        final Properties prop = new Properties();
+        try {
+            prop.load(HikariCPDataSource.class.getResourceAsStream("/application.properties"));
+        } catch (Exception e) {
+            logger.error("Error while loading application.properties file!", e);
+        }
+
+        config.setJdbcUrl("jdbc:mysql://localhost:3306/" + prop.getProperty("db.name"));
+        config.setUsername(prop.getProperty("db.username"));
+        config.setPassword(prop.getProperty("db.password"));
         ds = new HikariDataSource(config);
     }
 
