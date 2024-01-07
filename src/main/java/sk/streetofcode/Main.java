a63488df0242713e5f16611f681ff9711773c216
xjahic <jahic.jakub@gmail.com>
2024-01-07 13:58:28 +0100

    Create connection pool

--- src/main/java/sk/streetofcode/Main.java
+++ src/main/java/sk/streetofcode/Main.java
@@ -5,9 +5,8 @@ import java.sql.*;
 public class Main {
     public static void main(String[] args) {
         String select = "SELECT * FROM contact";
-        String connectionUrl = "jdbc:mysql://localhost:3306/contacts";
 
-        try (Connection conn = DriverManager.getConnection(connectionUrl, "root", "password");
+        try (Connection conn = HikariCPDataSource.getConnection();
              PreparedStatement ps = conn.prepareStatement(select);
              ResultSet rs = ps.executeQuery()) {
 
