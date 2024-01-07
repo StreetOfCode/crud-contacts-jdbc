853cbcd56ac68021fece861656e183291d30d22c
xjahic <jahic.jakub@gmail.com>
2024-01-07 13:53:30 +0100

    Connect to MySQL database and print contacts in Main

--- src/main/java/sk/streetofcode/Main.java
+++ src/main/java/sk/streetofcode/Main.java
@@ -1,7 +1,26 @@
 package sk.streetofcode;
 
+import java.sql.*;
+
 public class Main {
     public static void main(String[] args) {
-        System.out.println("Hello world!");
+        String select = "SELECT * FROM contact";
+        String connectionUrl = "jdbc:mysql://localhost:3306/contacts";
+
+        try (Connection conn = DriverManager.getConnection(connectionUrl, "root", "password");
+             PreparedStatement ps = conn.prepareStatement(select);
+             ResultSet rs = ps.executeQuery()) {
+
+            while (rs.next()) {
+                long id = rs.getLong("id");
+                String name = rs.getString("name");
+                String email = rs.getString("email");
+                String phoneNumber = rs.getString("phone");
+
+                System.out.println("id: " + id + ", name: " + name + ", email: " + email + ", phone number: " + phoneNumber);
+            }
+        } catch (SQLException e) {
+            System.out.println("Error while connecting to database!");
+        }
     }
 }
