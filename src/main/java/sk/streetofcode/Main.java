3b562bf2841440679dbe135e46978fcefa888f8d
xjahic <jahic.jakub@gmail.com>
2024-01-07 14:03:20 +0100

    Create packages, Contact and InputUtils

--- src/main/java/sk/streetofcode/Main.java
+++ src/main/java/sk/streetofcode/Main.java
@@ -1,25 +1,10 @@
 package sk.streetofcode;
 
-import java.sql.*;
+import sk.streetofcode.db.DBContactService;
 
 public class Main {
     public static void main(String[] args) {
-        String select = "SELECT * FROM contact";
-
-        try (Connection conn = HikariCPDataSource.getConnection();
-             PreparedStatement ps = conn.prepareStatement(select);
-             ResultSet rs = ps.executeQuery()) {
-
-            while (rs.next()) {
-                long id = rs.getLong("id");
-                String name = rs.getString("name");
-                String email = rs.getString("email");
-                String phoneNumber = rs.getString("phone");
-
-                System.out.println("id: " + id + ", name: " + name + ", email: " + email + ", phone number: " + phoneNumber);
-            }
-        } catch (SQLException e) {
-            System.out.println("Error while connecting to database!");
-        }
+        DBContactService service = new DBContactService();
+        service.readAll().forEach(System.out::println);
     }
 }
